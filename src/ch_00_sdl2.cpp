cf91e2e0ecba71c8c0f6172a15589b8354c68859
mikeybkats <michaelbkats@gmail.com>
2024-08-10 14:30:50 -0700

    adding sdl2

---
+++ src/ch_00_sdl2.cpp
@@ -0,0 +1,12 @@
+#include <SDL2/SDL.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+        std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+        return 1;
+    }
+
+    SDL_Quit();
+    return 0;
+}
