ff16669893f521a3a53d2c56cdd4bbf6f81d69d4
mikeybkats <michaelbkats@gmail.com>
2024-08-10 21:53:13 -0700

    just made a window with SDL2. YAY

--- src/ch_01_sdl2_window.cpp
+++ src/ch_01_sdl2_window.cpp
@@ -24,6 +24,27 @@ int main(int argc, char* args[]) {
 
     if (window == NULL) {
       printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    } else {
+      // Get window surface
+      screenSurface = SDL_GetWindowSurface(window);
+
+      // Fill the surface white
+      SDL_FillRect(screenSurface, NULL,
+                   SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+
+      // Update the surface
+      SDL_UpdateWindowSurface(window);
+
+      // Hack to get window to stay open
+      SDL_Event e;
+      bool quit = false;
+      while (!quit) {
+        while (SDL_PollEvent(&e)) {
+          if (e.type == SDL_QUIT) {
+            quit = true;
+          }
+        }
+      }
     }
   }
 }
