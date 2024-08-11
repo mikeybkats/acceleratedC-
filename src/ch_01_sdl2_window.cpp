3d8afb6b22945703cefd28cc892f42511269a5d6
mikeybkats <michaelbkats@gmail.com>
2024-08-10 21:28:12 -0700

    intellisense not showing warnings

--- src/ch_01_sdl2_window.cpp
+++ src/ch_01_sdl2_window.cpp
@@ -16,5 +16,14 @@ int main(int argc, char* args[]) {
   // initialize SDL
   if (SDL_Init(SDL_INIT_VIDEO) > 0) {
     printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+  } else {
+    // create window
+    window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED,
+                              SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                              SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (window == NULL) {
+      printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    }
   }
 }
