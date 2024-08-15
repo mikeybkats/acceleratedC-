ef2be76b97851757ee5983ebd21892b4919fcb5b
mikeybkats <michaelbkats@gmail.com>
2024-08-14 22:21:44 -0700

    adding exercises

---
+++ src/ch_02_4_complete_framing.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+void printRect(int v, int h) {
+  // const int size = 8;
+  // invariant we have written r rows so far
+  for (int r = 0; r < v; r++) {
+    int c = 0;
+    // invariant we have written c columns so far
+    while (c != h) {
+      if (r == 0 || r == v - 1) {
+        // print top or bottom edge
+        cout << "*";
+      } else {
+        if (c == 0 || c == h - 1) {
+          cout << "*";
+        } else {
+          cout << " ";
+        }
+      }
+      c++;
+    }
+    cout << endl;
+  }
+}
+
+void printTriangle(int height, int width) {
+  // invariant: we've printed r rows so far
+  for (int r = 0; r < height / 2; r++) {
+    // invariant: we've printed c cols so far
+    for (int c = 0; c < width; c++) {
+      if (c == width / 2 + r || c == width / 2 - r) {
+        cout << "*";
+      } else {
+        if (r == height / 2 - 1) {
+          cout << "*";
+        } else {
+          cout << " ";
+        }
+      }
+    }
+
+    cout << endl;
+  }
+}
+
+int main() {
+  printRect(8, 8);
+
+  printRect(8, 15);
+
+  printTriangle(14, 13);
+
+  return 0;
+}
